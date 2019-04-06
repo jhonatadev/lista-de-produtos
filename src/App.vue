c7c3307afd169200248de4c7dec99e82dc8007a1
jhonatadev <xxxonata@gmail.com>
2019-04-06 11:00:33 -0300

    Listado dados com Axios GET :octocat:

--- src/App.vue
+++ src/App.vue
@@ -31,10 +31,10 @@
             </tr>
           </thead>
           <tbody>
-            <tr>
-              <td>Arduino</td>
-              <td>100</td>
-              <td>50.00</td>
+            <tr v-for="produto of produtos" :key="produto.id">
+              <td>{{ produto.nome }}</td>
+              <td>{{ produto.quantidade }}</td>
+              <td>{{ produto.valor }}</td>
               <td>
                 <button class="waves-effect btn-small blue darken-1"><i class="material-icons">create</i></button>
                 <button class="waves-effect btn-small red darken-1"><i class="material-icons">delete_sweep</i></button>
@@ -47,6 +47,23 @@
 </template>
 
 <script>
+
+import Produto from './services/produtos'
+
+export default{
+  data(){
+    return {
+      produtos: []
+    }
+  },
+
+  mounted(){
+    Produto.listar().then(resposta => {
+      console.log(resposta.data)
+      this.produtos = resposta.data
+    })
+  }
+}
 
 </script>
 
